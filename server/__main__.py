189d0ab341a62a86cfa5f4e2827fb8951b65c31e
Sergey Safonov <Saf4ik85@gmail.com>
2019-08-04 18:59:53 +0300

    Lesson_6 completed

--- server/__main__.py
+++ server/__main__.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 import yaml
 import json
 import socket
+import logging
 
 from protocol import validate_request, make_response
 from actions import resolve
@@ -26,20 +27,44 @@ if args.config:
         file_config = yaml.load(file, Loader=yaml.Loader)
         config.update(file_config)
 
+
+# logger = logging.getLogger('main')
+# formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+# file_handler = logging.FileHandler('main.log')
+# stream_handler = logging.StreamHandler()
+
+# file_handler.setLevel(logging.DEBUG)
+# stream_handler.setLevel(logging.DEBUG)
+
+# file_handler.setFormatter(formatter)
+# stream_handler.setFormatter(formatter)
+
+# logger.addHandler(file_handler)
+# logger.addHandler(stream_handler)
+# logger.setLevel(logging.DEBUG)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers = [
+        logging.FileHandler('main.log'),
+        logging.StreamHandler(),
+    ]
+)
+
 host, port = config.get('host'), config.get('port')
-
-
 
 try:
     sock = socket.socket()
     sock.bind((host, port))
     sock.listen(5)
 
-    print(f'server started with { host }:{ port }')
+    logging.info(f'server started with { host }:{ port }')
 
     while True:
         client, address = sock.accept()
-        print(f'Client was detected { address[0] }:{ address[1] }')
+        logging.info(f'Client was detected { address[0] }:{ address[1] }')
 
         b_request = client.recv(config.get('buffersize'))
         
@@ -50,22 +75,20 @@ try:
             controller = resolve(action_name)
             if controller:
                 try:
-                    print(f'Client send valid request { request }')
+                    logging.info(f'Client send valid request { request }')
                     response = controller(request)
                 except Exception as err:
-                    print(f'Internal server error: { err }')
+                    logging.critical(f'Internal server error: { err }')
                     response = make_response(request, 500, data='Internal server error')
             else:
-                print(f'Controller with action_name {action_name} does not exists')
+                logging.error(f'Controller with action_name {action_name} does not exists')
                 response = make_response(request, 404, 'Action not found')
         else:
-            print(f'Client send invalid request { request }')
-            response = make_response(request, 404, 'Wrong request')
+            logging.error(f'Client send invalid request { request }')
+            response = make_response(request, 400, 'Wrong request')
         
         str_response = json.dumps(response)
         client.send(str_response.encode())            
-
-
 
 
 except KeyboardInterrupt:
